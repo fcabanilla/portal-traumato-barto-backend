f0b66f77579eb0b0c7b0bd8f6f4ae9bbf3105034
Federico Cabanilla <federico.cabanilla@gmail.com>
2020-01-06 16:53:22 -0300

    Terminado GET ALL de Poll

--- backend/api/controllers/polls.controller.js
+++ backend/api/controllers/polls.controller.js
@@ -4,11 +4,11 @@ const { query: sql } = require("../models/poll.model.json");
 const { DUPLICATE_ENTRY, NOT_SAVED, NOT_FOUND, PROCEDURE_NOT_FOUND, QUESTION_ANSWER_NOT_FOUND, EMPTY_POLL_NOT_FOUND } = require("../models/poll.error.model.json");
 
 module.exports = {
-    pollsControllerPost:    core.middleware([core.logRequest, create]),
-    pollsControllerGet:     core.middleware([core.logRequest, getAll]),
-    pollsControllerGetId:   core.middleware([core.logRequest, get]),
-    pollsControllerPut:     core.middleware([core.logRequest, update]),
-    pollsControllerDelete:  core.middleware([core.logRequest, deletePoll])
+    pollsControllerPost: core.middleware([core.logRequest, create]),
+    pollsControllerGet: core.middleware([core.logRequest, getAll]),
+    pollsControllerGetId: core.middleware([core.logRequest, get]),
+    pollsControllerPut: core.middleware([core.logRequest, update]),
+    pollsControllerDelete: core.middleware([core.logRequest, deletePoll])
 };
 
 function onlyNotUndefined(tmp) {
@@ -33,8 +33,8 @@ async function create(req, res) {
     const idProcedure = req.swagger.params.idProcedure.value;
     const query = sql.post;
     const { description, idEmptyPoll, typeOfPoll, pollDetails, idQuestion, idAnswer } = req.body;
-    
-    
+
+
     // const newPollDetail = {
     //     idQuestion,
     //     idAnswer
@@ -52,20 +52,20 @@ async function create(req, res) {
     try {
 
         const procedureDB = await pool.query(query[0], idProcedure);
-        if(!procedureDB.length) throw PROCEDURE_NOT_FOUND;
+        if (!procedureDB.length) throw PROCEDURE_NOT_FOUND;
 
         const emptyPollDB = await pool.query(query[2], idEmptyPoll);
         if (!emptyPollDB.length) throw EMPTY_POLL_NOT_FOUND;
 
-        const typeOfPollDB = await pool.query(query[3], newTypeOfPoll);
+        let typeOfPollDB = await pool.query(query[3], newTypeOfPoll);
 
-        if (!typeOfPollDB.length){
+        if (!typeOfPollDB.length) {
             const typeOfPollSaved = await pool.query(query[4], newTypeOfPoll);
             if (!typeOfPollSaved.affectedRows) throw NOT_SAVED;
-            const typeOfPollDB = await pool.query(query[3], newTypeOfPoll);
+            typeOfPollDB = await pool.query(query[3], newTypeOfPoll);
             if (!typeOfPollDB.length) throw NOT_FOUND;
         }
-        
+
         const { idtype_of_poll } = typeOfPollDB[0];
         newPoll.idtype_of_poll = idtype_of_poll;
 
@@ -74,16 +74,16 @@ async function create(req, res) {
 
         const pollDB = await pool.query(query[6], [description, idEmptyPoll, idProcedure, newPoll.idUser_Author, idtype_of_poll]);
         if (!pollDB.length) throw NOT_SAVED;
-        
+
         const { idpoll } = pollDB[0];
         for (const pollDetail of pollDetails) {
-            pollDetail.idpoll = idpoll;            
+            pollDetail.idpoll = idpoll;
             const { idAnswer, idQuestion } = pollDetail;
             const pollDetailDB = await pool.query(query[7], [idAnswer, idQuestion, idpoll]);
             if (!pollDetailDB.length) {
                 const pollDetailSaved = await pool.query(query[8], pollDetail);
                 if (!pollDetailSaved.affectedRows) throw NOT_SAVED;
-            }            
+            }
         }
         res.status(201).send({ message: "Se creo la Encuesta" });
 
@@ -94,7 +94,96 @@ async function create(req, res) {
 }
 async function getAll(req, res) {
     const query = sql.get;
+    const pollsDB = [];
     try {
+
+        const tmpPollsDB = await pool.query(query[0]);
+        if (!tmpPollsDB.length) throw NOT_FOUND;
+
+        for (const pollDB of tmpPollsDB) {
+
+
+
+
+            const tmpEmptyPollsDB = await pool.query(query[1], pollDB.idempty_poll);
+            if (!tmpEmptyPollsDB.length) throw NOT_FOUND;
+
+
+            for (const emptyPollDB of tmpEmptyPollsDB) {
+
+                const tmpEmptyPollDB = {
+                    idPoll: pollDB.idpoll,
+                    typeOfPoll: pollDB.type_of_poll_description,
+                    idEmptyPoll: emptyPollDB.idempty_poll,
+                    name: emptyPollDB.name,
+                    type: emptyPollDB.type,
+                    description: emptyPollDB.description,
+                    group: []
+                };
+                const tmpGroupsDB = await pool.query(query[2], tmpEmptyPollDB.idEmptyPoll);
+                if (!tmpGroupsDB.length) console.log('mocaso') // throw NOT_FOUND;
+
+                for (const groupDB of tmpGroupsDB) {
+                    const tmpGroupDB = {
+                        idGroup: groupDB.idquestion_group,
+                        name: groupDB.name,
+                        description: groupDB.description,
+                        subgroup: []
+                    };
+
+                    const tmpSubgroupsDB = await pool.query(query[3], tmpGroupDB.idGroup);
+                    if (!tmpSubgroupsDB.length) console.log('mocaso subgrupo') // throw NOT_FOUND;
+
+                    for (const subgroupDB of tmpSubgroupsDB) {
+                        const tmpSubgroupDB = {
+                            idSubgroup: subgroupDB.idquestion_subgroup,
+                            name: subgroupDB.name,
+                            description: subgroupDB.description,
+                            maxScore: subgroupDB.max_score,
+                            pollDetail: []
+                        };
+
+                        const tmpQuestionsDB = await pool.query(query[4], tmpSubgroupDB.idSubgroup);
+                        if (!tmpQuestionsDB.length) console.log('mocaso QUESION') // throw NOT_FOUND;
+                        for (const questionDB of tmpQuestionsDB) {
+                            const tmpPollDetailDB = {
+                                idQuestion: questionDB.idquestion,
+                                questionName: questionDB.name,
+                                question: questionDB.question
+                            };
+
+                            const tmpPollDetailsDB = await pool.query(query[5], [tmpPollDetailDB.idQuestion, tmpEmptyPollDB.idPoll ]);
+                            if(!tmpPollDetailsDB.length) console.log('Problemas Will Robinson!');
+
+                            const tmpQuestionDB = await pool.query(query[6], tmpPollDetailsDB[0].idanswer);
+                            if (!tmpQuestionDB.length) console.log('2 - Problemas Will Robinson!');
+
+                            tmpPollDetailDB.idAnswer = tmpQuestionDB[0].idanswer;
+                            tmpPollDetailDB.answer = tmpQuestionDB[0].answer;
+                            tmpPollDetailDB.score = tmpQuestionDB[0].score;
+
+
+                            console.log(tmpPollDetailDB);
+
+                            tmpSubgroupDB.pollDetail.push(tmpPollDetailDB);
+
+
+                            // tmpSubgroupDB.questions.push(tmpQuestionDB);
+                            // idQuestions.push(tmpQuestionDB.idQuestion);
+                        }
+
+
+
+
+                        tmpGroupDB.subgroup.push(tmpSubgroupDB);
+
+                    }
+                    tmpEmptyPollDB.group.push(tmpGroupDB);
+                }
+                pollsDB.push(tmpEmptyPollDB);
+            }
+        }
+        return res.status(200).send(pollsDB);
 
     } catch (err) {
         errorHandler(err, res);
